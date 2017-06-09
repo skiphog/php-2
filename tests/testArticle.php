3ab637159c6a290dc0bee08b4e94626dff35d136
Skip <skiphog@mail.ru>
2017-06-09 12:04:13 +0300

    add test

--- tests/testArticle.php
+++ tests/testArticle.php
@@ -6,3 +6,12 @@ require __DIR__ . '/../autoload.php';
 assert(Article::findById(1) instanceof Article);
 assert(is_array(Article::findAll()));
 assert(is_array(Article::findAllLatest(3)));
+
+$article = new Article();
+
+$article->title = 'title';
+$article->text = 'text';
+$article->author = 'author';
+
+assert(true === $article->save());
+assert(is_int($article->id));
