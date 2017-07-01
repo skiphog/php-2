6e07431a8c4ba1c69523b63de0ae12e4d7fc3b37
Skip <skiphog@mail.ru>
2017-07-01 20:23:13 +0300

    change readme

--- template/admin/particles/table.php
+++ template/admin/particles/table.php
@@ -12,6 +12,18 @@
                 </td>
             <?php endforeach; ?>
         </tr>
+        <tr>
+            <td>
+                <a href="//php-2/admin/news/edit?id=<?php echo $model->id; ?>"
+                        class="btn btn-outline-primary btn-sm">
+                    Редактировать
+                </a>
+                <a href="//php-2/admin/news/delete?id=<?php echo $model->id; ?>"
+                        class="btn btn-outline-danger btn-sm" onclick="return false">
+                    Удалить
+                </a>
+            </td>
+        </tr>
     <?php endforeach; ?>
 </table>
 
